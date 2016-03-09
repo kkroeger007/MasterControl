e0f144422e1c841e44de9443483f2ba1cfc6913b
patnolan33 <pat.nolan@heronsystems.com>
2016-03-09 09:43:29 -0500

    Update to Index File for Pushmenu
    Push menu for layer management is done on the right side now via button
    click.

--- Hawkeye_Android/app/src/main/assets/js/index_page.js
+++ Hawkeye_Android/app/src/main/assets/js/index_page.js
@@ -2,14 +2,12 @@
 
     // Global layers object:
     var g_layersObj = [];
-    //TODO: This was added to handle the stripped index value
-    var g_layersArray = [];
     $(document).ready(function() {
 
         // TODO: Pass the array in (from KEN):
         //var layerArray = ['LayerName1/Date1/Time1', 'LayerName1/Date2/Time2', 'Layername2/Date1/Time1', 'Layername2/Date2/Time2', 'Layername2/Date3/Time3', 'Layername3/Date1/Time1'];
         var layerArray = getAvailableLayers();
-        g_layersArray = layerArray;
+
         // Create the layers object to be used later:
         var layersObj = [];
         var layersIndex = 0;
@@ -62,7 +60,7 @@
         for (cardIndex = 0; cardIndex < layersObj.length; cardIndex++) {
             html += '<div class="mdl-card mdl-shadow--2dp">';
             html +=         '<div class="mdl-card__title">';
-            html +=             '<h2 class="mdl-card__title-text">' + layersObj[cardIndex].layerName + '</h2>';
+            html +=             '<h5 class="mdl-card__title-text">' + layersObj[cardIndex].layerName + '</h5>';
             html +=         '</div>';
 
             html +=         '<div class="mdl-card__menu">';
@@ -75,20 +73,21 @@
             html +=         '<div class="custom-collapse">';
             html +=             '<div class="measuringWrapper">';
             html +=                 '<div class="mdl-grid">';
-            html +=                     '<div class="mdl-cell mdl-cell--2-col">';
+            html +=                     '<div class="mdl-cell mdl-cell--6-col">';
             html +=                         '<p>' + layersObj[cardIndex].dates[0] + '</p>';
             html +=                     '</div>';
-            html +=                     '<div class="mdl-cell mdl-cell--10-col">';
+            html +=                     '<div class="mdl-cell mdl-cell--6-col">';
             var latestIdStr = layersObj[cardIndex].layerName + '/' + layersObj[cardIndex].dates[0] + '/' + layersObj[cardIndex].times[0];
-            html +=                         '<input id="' + latestIdStr + '" class="mdl-slider mdl-js-slider opacity-slider latest-layer-slider" type="range" min="0" max="100" value="0" tabindex="0">';
+            html +=                         '<input id="' + latestIdStr + '" class="mdl-slider mdl-js-slider opacity-slider latest-layer-slider" type="range" min="0" max="100" value="100" tabindex="0">';
             html +=                     '</div>';
             html +=                 '</div>';
+
             html +=             '</div>';
             html +=         '</div>';
 
 
             if(layersObj[cardIndex].dates.length > 1) {
-                html +=         '<button class="mdl-button mdl-js-button mdl-js-ripple-effect more-layers" disabled="true">';
+                html +=         '<button class="mdl-button mdl-js-button mdl-js-ripple-effect more-layers" style="display: none;">';
                 html +=             '<i class="material-icons custom-collapse-icon">keyboard_arrow_down</i>';
                 html +=         '</button>';
             }
@@ -98,10 +97,10 @@
 
             for (layerIndex = 1; layerIndex < layersObj[cardIndex].dates.length; layerIndex++) {
                 html +=                 '<div class="mdl-grid">';
-                html +=                     '<div class="mdl-cell mdl-cell--2-col">';
+                html +=                     '<div class="mdl-cell mdl-cell--6-col">';
                 html +=                         '<p>' + layersObj[cardIndex].dates[layerIndex] + '</p>';
                 html +=                     '</div>';
-                html +=                     '<div class="mdl-cell mdl-cell--10-col">';
+                html +=                     '<div class="mdl-cell mdl-cell--6-col">';
                 var idStr = layersObj[cardIndex].layerName + '/' + layersObj[cardIndex].dates[layerIndex] + '/' + layersObj[cardIndex].times[layerIndex];
                 html +=                         '<input id="' + idStr + '" class="mdl-slider mdl-js-slider opacity-slider" type="range" min="0" max="100" value="0" tabindex="0">';
                 html +=                     '</div>';
@@ -122,15 +121,8 @@
 
 
     function openMapLayers() {
-        $("#map").css("width", "75%");
-        $("#map").css("position", "absolute");
-        $("#map").css("top", "0");
-        $("#map").css("right", "0");
-
-        $(".maplayers-outer-container").css("display", "");
-        $(".preferences-outer-container").css("display", "none");
-
-        $(".mdl-layout__drawer-button").click();
+        $('.pushmenu-push').toggleClass('pushmenu-push-toright');
+        $('.pushmenu-left').toggleClass('pushmenu-open');
     }
 
     function openPreferences() {
@@ -139,7 +131,6 @@
         $("#map").css("top", "0");
         $("#map").css("right", "0");
 
-        $(".maplayers-outer-container").css("display", "none");
         $(".preferences-outer-container").css("display", "");
 
         $(".mdl-layout__drawer-button").click();
@@ -175,10 +166,35 @@
 
 
         $(".layer-toggle").on("change", function() {
-            var button = $(this).parent().parent().children('.more-layers');
-            button.prop('disabled', function() {
-                return !$(this).prop('disabled');
-            });
+
+            // Get sliders in card and all their values:
+            var sliders = $(this).parent().parent().find('.opacity-slider');
+            var layersArray = [];
+            var sliderTrueFalse = false;
+            for (sliderIndex = 0; sliderIndex < sliders.length; sliderIndex++) {
+                layersArray.push([sliders[sliderIndex].id, sliders[sliderIndex].value]);
+
+                if(sliderTrueFalse === false && sliderIndex > 0 && sliders[sliderIndex].value > 0) {
+                    sliderTrueFalse = true;
+                }
+            }
+
+            var forid = $(this).attr('for');
+            var checkbox = $('#'+$(this).attr('for'));
+            var sliderOnOff = checkbox.prop('checked');
+            var sliderID = checkbox.attr('id').replace('field-toggle-','');
+
+
+            if(g_layersObj[sliderID].dates.length > 1) {
+                var showButton = $(this).parent().next().next();
+                if(showButton.css('display') === 'none') {
+                    showButton.css('display', '');
+                } else {
+                    showButton.css('display', 'none');
+                }
+            }
+
+
 
             var latestExpandDiv = $(this).parent().next('.custom-collapse');
             if (latestExpandDiv.height() > 0) {
@@ -192,42 +208,47 @@
                 latestLayerSlider.get(0).MaterialSlider.change(100);
             }
 
+            var button = $(this).parent().parent().children('.more-layers');
             var expandDiv = button.next('.custom-collapse');
-            if (expandDiv.height() > 0 && button.prop('disabled') === true) {
+            if (expandDiv.height() > 0 && sliderOnOff === false) {
                 expandDiv.height(0);
+
+                if(button.find('.custom-collapse-icon').hasClass('custom-collapse-open')) {
+                    button.find('.custom-collapse-icon').toggleClass('custom-collapse-open');
+                }
             }
-            //TODO: We need a better from getting layer from checkbox change event....would like to be more dynamic rather than hardcoding field-toggle talk to Ben
-            var forid = $(this).attr('for');
-            var checkbox = $('#'+$(this).attr('for'));
-            var sliderVal = checkbox.prop('checked');
-            var sliderID = checkbox.attr('id').replace('field-toggle-','');
-            updateLayerOnOff(g_layersArray[sliderID],sliderVal);
+
+            if (sliderTrueFalse === true && sliderOnOff === true) {
+                button.click();
+            }
+
+            updateLayersOnOff(layersArray,sliderOnOff);
         });
+
+
 
         $('.opacity-slider').change(function() {
             var sliderVal = $(this).val();
             var sliderID = $(this).attr('id');
-            updateLayerOpacity(sliderID,sliderVal/100);
-            //var changedObj = {layer: sliderID, opacity: sliderVal/100};
-            //console.log(changedObj);
+            updateLayerOpacity(sliderID, sliderVal/100);
         });
-
 
 
         $(".back-button").click(function() {
             $("#map").css("width", "100%");
 
-            $(".maplayers-outer-container").css("display", "none");
             $(".preferences-outer-container").css("display", "none");
-
             $(".mdl-layout__drawer-button").click();
         });
 
         $(".check-button").click(function() {
             $("#map").css("width", "100%");
-
-            $(".maplayers-outer-container").css("display", "none");
             $(".preferences-outer-container").css("display", "none");
+        });
+
+        $(".map-layers-check").click(function() {
+            $('.pushmenu-push').toggleClass('pushmenu-push-toright');
+            $('.pushmenu-left').toggleClass('pushmenu-open');
         });
 
     };
